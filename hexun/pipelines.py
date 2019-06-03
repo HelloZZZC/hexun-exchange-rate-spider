e2a4c56d741ef07c8b13db53fbb872a437ede5fb
HelloZZZC <405954049@qq.com>
2019-06-03 18:01:04 +0800

    hexun部分汇率爬虫

---
+++ hexun/pipelines.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import time
+from hexun.util.db import DBConnection
+
+
+class HexunPipeline(object):
+    def process_item(self, item, spider):
+        sql = "INSERT INTO `exchange_rate` ( `from_country`, `to_country`, `rate`, `created_time`, `updated_time`) VALUES (%s, %s, %s, %s, %s)"
+        params = (item['from_country'], item['to_country'], item['rate'], int(time.time()), int(time.time()))
+        conn = DBConnection()
+        conn.execute(sql, params)
+        return item
---
+++ hexun/spiders/hexun_spider.py
@@ -0,0 +1,28 @@
+from scrapy.spiders import CrawlSpider
+from bs4 import BeautifulSoup
+from scrapy_splash import SplashRequest
+from hexun.items import HexunItem
+
+
+class HexunSpider(CrawlSpider):
+    name = 'hexun'
+    start_urls = ['http://forex.hexun.com/rmbhl']
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield SplashRequest(url, self.parse, args={'wait': 0.5})
+
+    def parse(self, response):
+        soup = BeautifulSoup(response.body, "html.parser")
+        td_list = soup.body.find(id='BankNameList').find_all('td', class_='fir_td')
+        for td in td_list:
+            item = HexunItem()
+            print(td.div.string.strip())
+            print(td.find(class_='pere').em.string.strip())
+            result = td.div.string.strip().split('/')
+            item['from_country'] = result[0]
+            item['to_country'] = result[1]
+            item['rate'] = td.find(class_='pere').em.string.strip()
+            yield item
+
+
